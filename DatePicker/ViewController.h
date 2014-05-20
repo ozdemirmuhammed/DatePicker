938ff9f0504297aab6b2bf9d44283c2825f4f553
ozdemirmuhammed <MacBook@Muhammed-MacBook-Pro.local>
2014-05-20 11:40:20 +0300

    Date Picker
    Date picker tutorial which is published in kod5.org

--- DatePicker/ViewController.h
+++ DatePicker/ViewController.h
@@ -9,5 +9,11 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *lblInfo1;
+@property (weak, nonatomic) IBOutlet UILabel *lblInfo2;
+@property (weak, nonatomic) IBOutlet UITextField *txtName;
+@property (weak, nonatomic) IBOutlet UITextField *txtSurname;
+@property (weak, nonatomic) IBOutlet UIDatePicker *datePick;
+- (IBAction)btnSave:(id)sender;
 
 @end
--- DatePicker/ViewController.m
+++ DatePicker/ViewController.m
@@ -18,6 +18,9 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    _txtName.delegate=self;
+    _txtSurname.delegate=self;
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +28,31 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)btnSave:(id)sender {
+    _lblInfo1.text=[_txtName.text stringByAppendingString:_txtSurname.text];
+    
+    NSDate *date = _datePick.date;
+    _lblInfo2.text = [date description];
+    
+    [_lblInfo1 resignFirstResponder];
+    [_lblInfo2 resignFirstResponder];
+}
+
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    
+    if (textField == _txtName || textField==_txtSurname) {
+        [textField resignFirstResponder];
+    }
+    else if (textField == _txtSurname) {
+        [_txtSurname resignFirstResponder];
+    }
+        
+        return NO;
+    
+}
+
+
 
 @end
